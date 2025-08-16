d55f1b8d299c8ed4a89374449ea4f0885e9c21f8
rafaelperales05 <rafaperales1229@gmail.com>
2025-08-16 12:05:09 -0500

    1. Added DVC 2. Finishes correcting dependcies and env 3. Setup struture for data_ingestion py structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup 
+
+
+setup(name='youtube', 
+    version='0.0.0', 
+    author='rafa', 
+    author_email= 'rafaperales1229@gmail.com',
+    packages=find_packages(), 
+    install_requires=[], 
+    
+)
